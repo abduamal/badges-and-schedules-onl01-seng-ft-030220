0c5db360c2d150350165b58eebbd7310893c7fa5
Amal Abdu <abduamal@ymail.com>
2020-03-06 06:54:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(names)
   return "Hello, my name is #{names}."
 end
+
+def batch_badge_creator(names)
+  badge_messages = []
+  names.each do |name|
+    badge_messages = badge_maker(name)
+  end
+  return badge_messages
+end
